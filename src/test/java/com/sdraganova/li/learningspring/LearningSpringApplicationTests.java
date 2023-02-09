516c2bcc92536a48e9224114d4b12c76d297bfc2
Zina Doneva <zina_doneva@smartcom.bg>
2023-02-09 18:59:53 +0200

    Initial commit

---
+++ src/test/java/com/sdraganova/li/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdraganova.li.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
